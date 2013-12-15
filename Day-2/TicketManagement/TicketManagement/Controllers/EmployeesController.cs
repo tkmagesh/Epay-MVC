ea209ab6fb4a603bc899234f210cc8093821eab0
tkmagesh <tkmagesh77@gmail.com>
2013-12-14 16:48:41 -0800

    Scaffolding Demo

---
+++ Day-2/TicketManagement/TicketManagement/Controllers/EmployeesController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TicketManagement.Models;
+
+namespace TicketManagement.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        //
+        // GET: /Employees/
+        private static List<Employee> employees = new List<Employee>();
+        public ActionResult Index()
+        {
+            return View(employees);
+        }
+
+        //
+        // GET: /Employees/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View(employees.FirstOrDefault(e => e.Id == id));
+        }
+
+        //
+        // GET: /Employees/Create
+
+        public ActionResult Create()
+        {
+            return View(new Employee());
+        }
+
+        //
+        // POST: /Employees/Create
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                if (this.ModelState.IsValid)
+                {
+                    employee.Id = employees.Any() ? employees.Max(e => e.Id) + 1 : 1;
+
+                    employees.Add(employee);
+
+                    return RedirectToAction("Index");
+                }
+                else {
+                    return View(employee);
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Employees/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View(employees.First(e => e.Id == id));
+        }
+
+        //
+        // POST: /Employees/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var emp = employees.First(e => e.Id == employee.Id);
+                emp.FirstName = employee.FirstName;
+                emp.LastName = employee.LastName;
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Employees/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View(employees.First(e => e.Id == id));
+        }
+
+        //
+        // POST: /Employees/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                employees.Remove(employees.First(e => e.Id == id));
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Day-2/TicketManagement/TicketManagement/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TicketManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage="No one in the world is without First Name")]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+
+    }
+}
